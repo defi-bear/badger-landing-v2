690cdef665d9056c30b9bf05834e3a8af7ef61be
Sajan Rajdev <25463788+sajanrajdev@users.noreply.github.com>
2021-02-18 22:51:57 -0500

    Added eslint/prettier packages and scripts
    Ran eslint -fix script: fixed style issues

--- src/index.tsx
+++ src/index.tsx
@@ -7,16 +7,16 @@ import Routes from './routes';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
-      <Routes />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root'),
+	<React.StrictMode>
+		<ThemeProvider theme={lightTheme}>
+			<Routes />
+		</ThemeProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
-  import.meta.hot.accept();
+	import.meta.hot.accept();
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,14 +5,14 @@ import Home from '../screens/Home';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-export default function() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route path={'/'} component={Home} />
-      </Switch>
-      <Footer />
-    </BrowserRouter>
-  )
+export default function () {
+	return (
+		<BrowserRouter>
+			<Header />
+			<Switch>
+				<Route path={'/'} component={Home} />
+			</Switch>
+			<Footer />
+		</BrowserRouter>
+	);
 }
