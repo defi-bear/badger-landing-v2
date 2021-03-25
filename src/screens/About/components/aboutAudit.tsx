3976e016dee668458890930c52d87b4232385448
Shing Ho Tan <talenteyedev@gmail.com>
2021-03-25 12:19:07 -0700

    overflow issue fix in about us page

--- src/screens/About/components/aboutAudit.tsx
+++ src/screens/About/components/aboutAudit.tsx
@@ -8,7 +8,8 @@ import { isMobile } from 'react-device-detect';
 
 const useStyles = makeStyles((theme) => ({
     main: {
-        marginTop: isMobile ? 30 : 110
+        marginTop: isMobile ? 30 : 110,
+        overflowX: 'hidden'
     },
     leftPanel: {
         backgroundColor: theme.palette.secondary.main,
